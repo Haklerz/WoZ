fddbd8c4491ba6ed300033ec7f4036c8e1d683c2
Haklerz <haakon.lervik@gmail.com>
2019-02-19 11:01:07 +0100

    changed info to descripton

--- src/Item.java
+++ src/Item.java
@@ -5,18 +5,18 @@ package src;
  * With this class you can:
  * <ul>
  * <li>Set the name of the item.
- * <li>Set the info on the item.
+ * <li>Set the description on the item.
  * <li>Get the name of the item.
- * <li>Get the info on the item.
+ * <li>Get the description on the item.
  * </ul>
- * The name and the info are of the type<code>String</code>.
+ * The name and the description are of the type<code>String</code>.
  * 
  * @author Håkon "Haklerz" Lervik
  * @version 1.0
  */
 public class Item {
     private String name;
-    private String info;
+    private String description;
 
     /**
      * Sets the name of the item. Takes a<code>String</code>.
@@ -34,17 +34,17 @@ public class Item {
     }
     
     /**
-     * Sets the info on the item. Takes a<code>String</code>.
+     * Sets the description on the item. Takes a<code>String</code>.
      */
-    public void setInfo(String info) {
-        this.info = info;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     /**
-     * Returns the info on the item as a<code>String</code>.
-     * @return Info on the item.
+     * Returns the description on the item as a<code>String</code>.
+     * @return Description on the item.
      */
-    public String getInfo() {
-        return this.info;
+    public String getDescription() {
+        return this.description;
     }
 }
--- src/Room.java
+++ src/Room.java
@@ -2,22 +2,34 @@ package src;
 
 import java.util.HashMap;
 
+/**
+ * Represents a room.
+ */
 public class Room {
-    private String info;
+    private String description;
     private HashMap<String, Room> exits;
     private ItemCollection items;
 
-    public Room(String info) {
-        this.info = info;
+    /**
+     * Creates a room.
+     * @param description Description of the room.
+     */
+    public Room(String description) {
+        this.description = description;
         this.exits = new HashMap<>();
     }
 
+    /**
+     * Adds an exit to another room.
+     * @param direction Direction to the room.
+     * @param neighbor Neighboring room.
+     */
     public void addExit(String direction, Room neighbor)  {
         this.exits.put(direction, neighbor);
     }
 
-    public String getInfo() {
-        return this.info;
+    public String getDescription() {
+        return this.description;
     }
 
     public Item getItem(String item) {
